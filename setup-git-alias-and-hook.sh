c9ae4550c8366ffd4c84778faa4de1b61d5581ad
Beng Eu <beng@bengeu.com>
2023-09-10 21:20:25 +0800

    Add informational messages during setup

--- setup-git-alias-and-hook.sh
+++ setup-git-alias-and-hook.sh
@@ -2,16 +2,18 @@
 alias=${1:-rau}
 script_name=add-auto-detected-upstream.sh
 local_script=$(dirname "$0")/$script_name
-adding_alias_message="Adding '$alias' Git alias for one-liner transformation of"
+set_alias_message="Setting '$alias' Git alias for one-liner transformation of"
 
 if [ -f "$local_script" ]; then
   script=$(cat "$local_script")
-  echo "$adding_alias_message $local_script"
+  echo "$set_alias_message $local_script"
 else
   remote_script="https://raw.githubusercontent.com/thebengeu/auto-git-remote-add-upstream/master/$script_name"
   script=$(curl -s "$remote_script")
-  echo "$adding_alias_message $remote_script"
+  echo "$set_alias_message $remote_script"
 fi
+
+echo
 
 git config --global alias."$alias" "!$(
   echo "$script" |
@@ -28,18 +30,23 @@ template_dir=$(git config --global init.templateDir)
 if [ "$template_dir" = "" ]; then
   XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
   template_dir="$XDG_CONFIG_HOME/git/template"
+  echo "Setting template directory to $template_dir in global Git config"
+  echo
   git config --global init.templateDir "$template_dir"
 fi
 
 command="git $alias"
 hooks_dir="$template_dir/hooks"
-post_checkout="$hooks_dir/post-checkout"
+post_checkout_path="$hooks_dir/post-checkout"
 
 mkdir -p "$hooks_dir"
 
-if [ ! -f "$post_checkout" ]; then
-  echo '#!/usr/bin/env sh' >"$post_checkout"
-  chmod +x "$post_checkout"
+if [ ! -f "$post_checkout_path" ]; then
+  echo '#!/usr/bin/env sh' >"$post_checkout_path"
+  chmod +x "$post_checkout_path"
 fi
 
-grep -Fqx "$command" "$post_checkout" || echo "$command" >>"$post_checkout"
+grep -Fqx "$command" "$post_checkout_path" || echo "$command" >>"$post_checkout_path"
+
+echo "$post_checkout_path now contains:"
+cat "$post_checkout_path"
