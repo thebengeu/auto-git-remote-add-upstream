bf79694fb82ada76f337cb469d991675de72433a
Beng Eu <beng@bengeu.com>
2023-09-11 01:34:48 +0800

    Use script copied to template dir, run during post-checkout once then remove

--- setup-git-alias-and-hook.sh
+++ setup-git-alias-and-hook.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/env sh
 alias=${1:-rau}
-script_name=add-upstream-auto-detected-url.sh
-local_script=$(dirname "$0")/$script_name
-set_alias_message="Setting '$alias' Git alias for one-liner transformation of"
-
-if [ -f "$local_script" ]; then
-  script=$(cat "$local_script")
-  echo "$set_alias_message $local_script"
-else
-  remote_script="https://raw.githubusercontent.com/thebengeu/auto-git-remote-add-upstream/master/$script_name"
-  script=$(curl -s "$remote_script")
-  echo "$set_alias_message $remote_script"
-fi
-
-echo
-
-git config --global alias."$alias" "!$(
-  echo "$script" |
-    # Strip shebang line
-    tail -n +2 |
-    # Strip leading spaces, commented lines and blank lines
-    sed -e 's/^ *//' -e '/^\(#.*\)\{0,1\}$/d' |
-    # Join lines with newline
-    paste -sd '\n' -
-) #"
-
 template_dir=$(git config --global init.templateDir)
 
 if [ "$template_dir" = "" ]; then
@@ -35,18 +10,40 @@ if [ "$template_dir" = "" ]; then
   git config --global init.templateDir "$template_dir"
 fi
 
-command="git $alias"
 hooks_dir="$template_dir/hooks"
-post_checkout_path="$hooks_dir/post-checkout"
+script_name=add-upstream-auto-detected-url.sh
+script_dest_path="$hooks_dir/$script_name"
+local_script=$(dirname "$0")/$script_name
 
 mkdir -p "$hooks_dir"
+
+if [ -f "$local_script" ]; then
+  cp "$local_script" "$hooks_dir"
+  echo "Copied $local_script to $hooks_dir"
+else
+  remote_script="https://raw.githubusercontent.com/thebengeu/auto-git-remote-add-upstream/master/$script_name"
+  curl -s "$remote_script" >"$script_dest_path"
+  echo "Downloaded $remote_script to $script_dest_path"
+fi
+
+echo
+
+chmod +x "$script_dest_path"
+
+post_checkout_path="$hooks_dir/post-checkout"
 
 if [ ! -f "$post_checkout_path" ]; then
   echo '#!/usr/bin/env sh' >"$post_checkout_path"
   chmod +x "$post_checkout_path"
 fi
 
-grep -Fqx "$command" "$post_checkout_path" || echo "$command" >>"$post_checkout_path"
+grep -q "$script_name" "$post_checkout_path" ||
+  echo ".git/hooks/$script_name && sed -i'' /$script_name/d .git/hooks/post-checkout" >>"$post_checkout_path"
 
 echo "$post_checkout_path now contains:"
 cat "$post_checkout_path"
+echo
+
+git config --global alias."$alias" "!$script_dest_path"
+
+echo "Set '$alias' Git alias to '!$script_dest_path'"
